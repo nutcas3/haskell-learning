4975ef9615512ca5d09e86c51beed66e9661c53b
nutcas3 <mauricee423@gmail.com>
2024-09-28 13:08:49 +0300

    boolean

--- datatypesfunctions/src/DatatypesFunctions.hs
+++ datatypesfunctions/src/DatatypesFunctions.hs
@@ -3,3 +3,16 @@ module DatatypesFunctions where
 import           Prelude hiding (elem, filter, length, lookup, not, reverse,
                           (++), (||))
 
+not :: Bool -> Bool
+not True = False
+not False = True
+
+(||) :: Bool -> Bool -> Bool
+(||) True  _ = True
+(||) False y = y
+
+(|||) :: Bool -> Bool -> Bool
+(|||) True  True  = True
+(|||) True  False = True
+(|||) False True  = True
+(|||) False False = False
